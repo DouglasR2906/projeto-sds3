4b9abdc7d0c1bae16da96c51e1ed8bfd7e95b458
Douglas R. Oliveira <douglasr.comp@hotmail.com>
2021-05-07 20:31:39 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,20 @@
+import Dashborad from 'pages/Dashboard';
+import Home from 'pages/Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/dashboard" exact>
+                    <Dashborad />
+                </Route>
+            </Switch> 
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
